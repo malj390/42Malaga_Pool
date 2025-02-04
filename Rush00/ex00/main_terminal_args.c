be18e882d33a47ace1a64379fb53750d6e454a21
Miguel Angel Lermo Jimenez <mlermo-j@c1r1s5.42malaga.com>
2025-02-04 19:57:20 +0100

    my_atoi added to add extra functionality

--- Rush00/ex00/main_terminal_args.c
+++ Rush00/ex00/main_terminal_args.c
@@ -1,21 +1,37 @@
 /* ************************************************************************** */
 /*   main.c                                                                   */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_terminal_args.c                               :+:      :+:    :+:   */
 /*   By: sfedosee <sfefosee@student.42malaga>         +:+ +:+         +:+     */
 /*   By: mlermo-j <mlermo-j@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 20:49:52 by mlermo-j          #+#    #+#             */
-/*   Updated: 2025/02/04 18:54:54 by mlermo-j         ###   ########.fr       */
+/*   Updated: 2025/02/04 19:56:30 by mlermo-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h> \\ From here comes the original function atoi
 
 int	rush(int x, int y);
 
-int	main(void)
+int my_atoi(char *p) {
+  int k = 0;
+  while (*p) {
+    k = k*10 + (*p) - '0';
+    p++;
+  }
+  return k;
+}
+
+
+int	main(int argc, char **argv)
 {
-	rush(5, 3);
+	if (argc == 3)
+	{
+		int a = my_atoi(argv[1]);
+		int b = my_atoi(argv[2]);
+		rush(a, b);
+	}
 	return (0);
 }
