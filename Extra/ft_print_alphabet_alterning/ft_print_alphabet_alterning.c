68bc22abb378108342c131d74a76c1e54ce5c56c
Miguel Angel Lermo Jimenez <mlermo-j@c1r3s5.42malaga.com>
2025-02-07 13:26:43 +0100

    update

--- Extra/ft_print_alphabet_alterning/ft_print_alphabet_alterning.c
+++ Extra/ft_print_alphabet_alterning/ft_print_alphabet_alterning.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet_alterning.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlermo-j <mlermo-j@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 13:23:57 by mlermo-j          #+#    #+#             */
+/*   Updated: 2025/02/07 13:25:52 by mlermo-j         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	putchar(char c)
@@ -5,12 +17,12 @@ void	putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet()
+void	ft_print_alphabet(void)
 {
-	int	i;
+	char	i;
 
 	i = 'a';
-	while(i <= 'z')
+	while (i <= 'z')
 	{
 		if (i % 2 == 0)
 		{
@@ -24,7 +36,8 @@ void	ft_print_alphabet()
 	}
 }
 
-int main(void)
+int	main(void)
 {
 	ft_print_alphabet();
+	return (0);
 }
